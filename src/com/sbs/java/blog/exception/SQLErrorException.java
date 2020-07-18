d89fe0532b85b0c5e0f3c08607029e0b4cfa1b11
rntaos77 <rntaos77@gmail.com>
2020-07-18 15:43:17 +0900

    MySQL 오류 메세지가 브라우저 화면에 나오도록 수정

--- src/com/sbs/java/blog/exception/SQLErrorException.java
+++ src/com/sbs/java/blog/exception/SQLErrorException.java
@@ -1,7 +1,14 @@
 package com.sbs.java.blog.exception;
 
 public class SQLErrorException extends RuntimeException {
-	public SQLErrorException(String message) {
+	private Exception origin;
+
+	public SQLErrorException(String message, Exception origin) {
 		super(message);
+		this.origin = origin;
+	}
+	
+	public Exception getOrigin() {
+		return origin;
 	}
 }
