19b95da20a117d33c8908b1d583f029660c1d9ac
rntaos77 <rntaos77@gmail.com>
2020-07-22 01:25:13 +0900

    액션에서 req, resp의미없이 받는 구조 없애기, 세션 정보를 모든 JSP에서, 모든 JS에서 편하게 쓸 수 있도록, beforeAction과 head.jspf에서 작업

--- src/com/sbs/java/blog/controller/Controller.java
+++ src/com/sbs/java/blog/controller/Controller.java
@@ -5,30 +5,51 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sbs.java.blog.dto.CateItem;
+import com.sbs.java.blog.dto.Member;
 import com.sbs.java.blog.service.ArticleService;
+import com.sbs.java.blog.service.MemberService;
 
 public abstract class Controller {
 	protected Connection dbConn;
 	protected String actionMethodName;
 	protected HttpServletRequest req;
 	protected HttpServletResponse resp;
+	protected HttpSession session;
 	
 	protected ArticleService articleService;
+	protected MemberService memberService;
 	
 	public Controller(Connection dbConn, String actionMethodName, HttpServletRequest req, HttpServletResponse resp) {
 		this.dbConn = dbConn;
 		this.actionMethodName = actionMethodName;
 		this.req = req;
+		this.session = req.getSession();
 		this.resp = resp;
 		articleService = new ArticleService(dbConn);
+		memberService = new MemberService(dbConn);
 	}
 	
 	public void beforeAction() {
 		List<CateItem> cateItems = articleService.getForPrintCateItems();
 		
 		req.setAttribute("cateItems", cateItems);
+		
+		int loginedMemberId = -1;
+		boolean isLogined = false;
+		Member loginedMember = null;
+		
+		if(session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int)session.getAttribute("loginedMemberId");
+			isLogined = true;
+			loginedMember = memberService.getMemberById(loginedMemberId);
+		}
+		
+		req.setAttribute("loginedMemberId", loginedMemberId);
+		req.setAttribute("isLogined", isLogined);
+		req.setAttribute("loginedMember", loginedMember);
 	}
 	
 	public void afterAction() {
--- src/com/sbs/java/blog/controller/HomeController.java
+++ src/com/sbs/java/blog/controller/HomeController.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 public class HomeController extends Controller{
 	public HomeController(Connection dbConn, String actionMethodName, HttpServletRequest req,
 			HttpServletResponse resp) {
